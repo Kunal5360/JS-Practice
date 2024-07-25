107443a0e5ac6c47e59c54ba31304d6023027093
Kunal5360 <kunalnagdawane323@gmail.com>
2024-07-25 15:39:20 +0530

    conversion

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let score = null;
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+//33 ==> 33 "number"
+//33abc ==> NaN (Not a Number)
+//undefined ==> NaN
+//null ==> 0
+
+let isLoggedIn = 1;
+let boolenLoggedIn = Boolean(isLoggedIn);
+//console.log(boolenLoggedIn);
+
+// 1 --> true; 0 --> false
+//"" --> false
+//"Kunal" --> true
+
+let someNumber = 33;
+let convertString = String(someNumber);
+console.log(convertString);
+console.log(typeof convertString);
+console.log("Important Update");
